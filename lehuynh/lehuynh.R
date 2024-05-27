bc2a5c140f507ba84a28f4a71eee94fa6cbc55c5
le-huynh <lehuynh200194@gmail.com>
2024-05-27 14:56:56 +0700

    add logo for `lehuynh` Rpackage

---
+++ lehuynh/lehuynh.R
@@ -0,0 +1,21 @@
+
+library(here)
+library(hexSticker)
+
+sticker(here("lehuynh/github_contribution.png"),
+        package = "lehuynh",
+        p_size = 25,
+        s_x = 1,
+        s_y = .75,
+        s_width = .6,
+        h_fill = "#0D1017",
+        h_color = "#3DE65A",
+        filename = "lehuynh/lehuynh.png")
+
+
+
+
+
+
+
+
