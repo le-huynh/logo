4b14643e4f1973052346b8a4a4f187178b51ba9f
le-huynh <lehuynhtrucly.hcmus@gmail.com>
2025-01-19 12:09:39 -0500

    `chva.extras` Rpackage: update color of pkg name

--- chva.extras/chva.extras.R
+++ chva.extras/chva.extras.R
@@ -4,11 +4,10 @@ library(hexSticker)
 
 sticker(here("chva.extras/va_map.png"),
         package = "chva.extras",
-        p_size = 20,
+        p_size = 22,
         s_x = 1,
         s_y = .75,
         s_width = 1.5,
-        p_color = "#E57200",
         h_fill = "#232D4B",
         h_color = "#E57200",
         filename = "chva.extras/chva.extras.png")
