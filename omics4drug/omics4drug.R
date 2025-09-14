239ba6d73b052ebdc60cfb7b7ca179e5e4285a15
le-huynh <lehuynhtrucly.hcmus@gmail.com>
2025-09-14 13:52:45 -0400

    `omics4drug` initial logo

---
+++ omics4drug/omics4drug.R
@@ -0,0 +1,23 @@
+
+library(here)
+library(hexSticker)
+
+sticker(here("omics4drug/fig.png"),
+        package = "omics4drug",
+        p_size = 22,
+        p_color = "#960018",
+        s_x = 1,
+        s_y = .75,
+        s_width = 0.55,
+        h_fill = "cornsilk",
+        h_color = "#960018",
+        h_size = 1,
+        filename = "omics4drug/omics4drug.png")
+
+
+
+
+
+
+
+
